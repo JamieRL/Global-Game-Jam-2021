37aed6fec0b9f510aa7f1abd5d952f95ada8d9fc
JamieRL <lee.jamr@gmail.com>
2021-01-30 13:56:30 -0800

    Add GameManager and Health scripts. Put health script on player prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager gm = null;
+	public bool isGameOver = true;
+	public string SceneToLoad = "";
+
+	private void Awake() {
+		if(gm == null) {
+			gm = this;
+		} else if(gm != this) {
+			Destroy(gameObject);
+		}
+
+		if(SceneToLoad =="") {
+			SceneToLoad = SceneManager.GetActiveScene().name;
+		}
+	}
+
+	private void reloadGame() {
+		SceneManager.LoadScene(SceneToLoad);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+	public int health;
+	public int numLives;
+	public bool isAlive = true;
+	public int respawnHealthPoints;
+	private Vector2 respawnPosition;
+	public string LevelToLoad = "";
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		respawnPosition = transform.position;
+
+		if(LevelToLoad == "") {
+			LevelToLoad = SceneManager.GetActiveScene().name;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(GameManager.gm) {
+			if(GameManager.gm.isGameOver) {
+				return;
+			}
+		}
+
+		if(health <= 0) {
+			if(isAlive) {
+				Die();
+			}
+		}
+	}
+	public virtual void Die() {
+		numLives = numLives - 1;
+		isAlive = false;
+		if(numLives < 0) {
+			GameOver();
+		}
+	}
+	public void damageHealth(int damage) {
+		health -= damage;
+	}
+
+	public void heal(int healing) {
+		health += healing;
+	}
+
+	private void GameOver() {
+		SceneManager.LoadScene(LevelToLoad);
+	}
+}
