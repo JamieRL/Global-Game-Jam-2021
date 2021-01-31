cdcb9de5804b9b70a9a0fb580daba9204a394976
Yasmeen Akbari <yasmeen.akbari@gmail.com>
2021-01-31 11:33:58 -0800

    Update PlayerPlatformerController.cs

--- Assets/Scripts/PlayerPlatformerController.cs
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -9,13 +9,13 @@ public class PlayerPlatformerController : PhysicsObject {
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
-    // private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
     // private Animator animator;
 
     // Use this for initialization
     void Awake () 
     {
-        // spriteRenderer = GetComponent<SpriteRenderer> ();    
+        spriteRenderer = GetComponent<SpriteRenderer> ();    
         // animator = GetComponent<Animator> ();
     }
 
@@ -34,11 +34,11 @@ public class PlayerPlatformerController : PhysicsObject {
             }
         }
 
-        // bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        // if (flipSprite) 
-        // {
-        //     spriteRenderer.flipX = !spriteRenderer.flipX;
-        // }
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0) : (move.x < 0));
+        if (flipSprite) 
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
 
         // animator.SetBool ("grounded", grounded);
         // animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
