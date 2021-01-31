7e3b95f42ef79da83f259611bea918c31cc09e0c
JamieRL <lee.jamr@gmail.com>
2021-01-31 13:27:49 -0800

    Set up test scene add moving platform animation

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Player on the platform");
+		if(other.gameObject.tag == "Player") {
+			other.transform.parent = transform;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.tag == "Player") {
+			other.transform.parent = null;
+		}
+	}
+
+	void Update()
+	{
+
+	}
+}
